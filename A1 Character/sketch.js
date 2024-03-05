45ca38301a164a6d7b70d5576525661e4fc88f4f
chinonye-richmond <chinonyerem.richmond@rcsd.ca>
2024-03-05 12:08:18 -0600

    character

---
+++ A1 Character/sketch.js
@@ -0,0 +1,31 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(400,700);
+}
+
+function draw() {
+  background(220);
+  fill("green");
+  triangle(200,300,150,550,250,550);
+  fill("black")
+  circle(200,330,110)
+  fill(210,180,150);
+  circle(200,350,90);
+  rectMode(CENTER);
+  strokeWeight(3)
+  line(230,550,230,650);  
+  line(170,550,170,650);
+  fill("red");
+  arc(170,650,25,15,TWO_PI, PI,CHORD);
+  arc(230,650,25,15,TWO_PI, PI,CHORD);
+  line(175,425,)
+
+ 
+}
