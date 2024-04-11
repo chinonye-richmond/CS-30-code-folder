f91ad02362dcdea9307a86f1fe4a6baf3af7a5f4
chinonye-richmond <chinonyerem.richmond@rcsd.ca>
2024-04-11 11:22:41 -0600

    big or small

---
+++ BigOrSmall/src/BigOrSmall.java
@@ -0,0 +1,19 @@
+// Big or Small
+// Chinonye
+// 4-11-2024
+
+public class BigOrSmall
+{
+	public static void check( int a, int b ){		
+        if (a>b){
+            System.out.println("yes");
+        }
+        else if (a<b){
+            System.out.println("no");
+        }
+        else {
+            System.out.println("equals");
+        }
+		
+	}
+}
