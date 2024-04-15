7b77c217fea8b42c0a9cc6b22b1267658bd30324
chinonye-richmond <chinonyerem.richmond@rcsd.ca>
2024-04-15 11:49:41 -0600

    comments

--- a10/src/SumRunner.java
+++ a10/src/SumRunner.java
@@ -1,3 +1,10 @@
+// Chinonye Richmond
+// Assignment 10
+// 4-15-2024
+// this program calls the methods from the file sum
+// It adds up and prints out the sum of two numbers.
+
+
 public class SumRunner {
     public static void main(String[] args) throws Exception {
         Sum number = new Sum();
