3317a97d155e1f3f9a363e8386204c48452dc748
chinonye-richmond <chinonyerem.richmond@rcsd.ca>
2024-04-15 11:47:13 -0600

    a10

---
+++ a10/src/SumRunner.java
@@ -0,0 +1,9 @@
+public class SumRunner {
+    public static void main(String[] args) throws Exception {
+        Sum number = new Sum();
+        number.setNums(5,9);
+        number.sum();
+        number.print();
+
+    }
+}
