5e55f12f5c35e87292f1a2686fd72d815c9f29e3
chinonye-richmond <chinonyerem.richmond@rcsd.ca>
2024-04-16 12:09:07 -0600

    c to f

--- a12/src/Fahrenheit.java
+++ a12/src/Fahrenheit.java
@@ -1,8 +1,13 @@
 public class Fahrenheit {
     private double fahrenheit;
+    private double celsius;
+
 
 	public void setFahrenheit(double fahren){
         fahrenheit = fahren;
+    }
+    public void setCelsius(double cel){
+        celsius = cel;
     }
 	
 	public double getCelsius()
@@ -12,7 +17,18 @@ public class Fahrenheit {
 		return celsius;
 	}
 
-	public void print(){
+    public double getFahrenheit(){
+        double fahrenheit = (celsius *(9.0/5.0))+ 32.0;
+        return fahrenheit;
+
+    }
+
+	public void printC(){
 		System.out.println(getCelsius() + " Celsius");
 	}
+
+    public void printF(){
+		System.out.println(getFahrenheit() + " Fahrenheit");
+	}
 }
+
--- a12/src/FahrenheitRunner.java
+++ a12/src/FahrenheitRunner.java
@@ -5,9 +5,14 @@
 
 public class FahrenheitRunner {
     public static void main(String[] args) throws Exception {
-        Fahrenheit test = new Fahrenheit();
-        test.setFahrenheit(98.6);
-        test.getCelsius();
-        test.print();
+        Fahrenheit test1 = new Fahrenheit();
+        test1.setFahrenheit(98.6);
+        test1.getCelsius();
+        test1.printC();
+
+        Fahrenheit test2 = new Fahrenheit();
+        test2.setCelsius(37.0);
+        test2.getFahrenheit();
+        test2.printF();
     }
 }
