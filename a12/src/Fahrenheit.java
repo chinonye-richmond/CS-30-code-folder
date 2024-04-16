7b13f1b2b9d82edbbb020a9e4a17f272a4b90fc8
chinonye-richmond <chinonyerem.richmond@rcsd.ca>
2024-04-16 11:58:41 -0600

    a12

---
+++ a12/src/Fahrenheit.java
@@ -0,0 +1,17 @@
+public class Fahrenheit {
+    private double fahrenheit;
+
+	public void setFahrenheit(double fahren){
+        fahrenheit = fahren;
+    }
+	
+	public double getCelsius()
+	{
+		double celsius = (fahrenheit - 32.0)*(5.0/9.0);
+		return celsius;
+	}
+
+	public void print(){
+		System.out.println(getCelsius() + " Celsius");
+	}
+}
---
+++ a12/src/FahrenheitRunner.java
@@ -0,0 +1,8 @@
+public class FahrenheitRunner {
+    public static void main(String[] args) throws Exception {
+        Fahrenheit test = new Fahrenheit();
+        test.setFahrenheit(98.6);
+        test.getCelsius();
+        test.print();
+    }
+}
