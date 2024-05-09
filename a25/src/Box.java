313e12aa102787943f4846981b59c614cb7e63fd
chinonye-richmond <chinonyerem.richmond@rcsd.ca>
2024-05-09 11:47:02 -0600

    a25

---
+++ a25/src/Box.java
@@ -0,0 +1,11 @@
+public class Box {
+    public void box(String d){
+        int index = d.indexOf(0);
+        int length = d.length();
+        for (index = 0; index < length; index++){
+            System.out.println(d);
+        }
+
+    }
+
+}
---
+++ a25/src/BoxRunner.java
@@ -0,0 +1,10 @@
+// Chinonye Richmond
+// Word Loop
+// 5/9/2024
+
+public class BoxRunner {
+    public static void main(String[] args) throws Exception {
+        Box test = new Box();
+        test.box("hibiscus");
+    }
+}
