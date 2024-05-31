8422ec00f1fa4693961e68a98137d63987132516
chinonye-richmond <chinonyerem.richmond@rcsd.ca>
2024-05-31 11:50:56 -0600

    m27

--- m27/src/GroceryList.java
+++ m27/src/GroceryList.java
@@ -4,28 +4,41 @@ import java.util.Scanner;
 public class GroceryList {
     public static void main(String[] args) throws Exception {
         Scanner keys = new Scanner(System.in);
-        
-        System.out.println("What do you want to add to your grocery list? ");
-        String one = keys.nextLine();
-
         List<String> namesList = new ArrayList<String>();
-        namesList.add(one);
-
-        System.out.println("Grocery List: \n" + namesList);
+        boolean running = true;
 
 
-
-        System.out.println("Do you have more items to add? ");
-        String more = keys.nextLine();
-        
-        if (more == "yes"){
+        while (running) {
+            
             System.out.println("What do you want to add to your grocery list? ");
-            String two = keys.nextLine();
-            namesList.add(two);
-            System.out.println("Grocery List: \n" + namesList);
-        } 
-        else if (more == "no") {
-            System.out.println("Grocery List: \n" + namesList); 
+            String one = keys.nextLine();
+
+            namesList.add(one);
+
+
+
+            System.out.println("Your grocery list currently is: ");
+
+            for(int i = 0; i < namesList.size(); i++){
+                System.out.println(namesList.get(i));
+            }
+                        
+            System.out.println("Do you have more items to add? ");
+            String more = keys.nextLine();
+
+            if (more.equals("yes")){
+                running = true;
+            } 
+            else {
+                running = false;
+            }
+        }
+
+        System.out.println("");
+        System.out.println("Your grocery list is: ");
+
+        for(int i = 0; i < namesList.size(); i++){
+            System.out.println(namesList.get(i));
         }
 
 
